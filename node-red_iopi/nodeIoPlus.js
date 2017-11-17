988a2ecaa86a0a252842da48b405c044a61cd055
Koen Luyten <koenluyten@hotmail.com>
2017-11-17 17:50:48 +0100

    added ioPlusIn

--- node-red_iopi/nodeIoPlus.js
+++ node-red_iopi/nodeIoPlus.js
@@ -2,104 +2,125 @@
 module.exports = function (RED) {
 	"use strict";
 	var settings = RED.settings;
-	//var events = require("events");
+	var events = require("events");
 	var Iopi = require("./iopi");
 
+	// config
 	function nodeIoPlus(config) {
 		RED.nodes.createNode(this, config);
 		var node = this;
-		node.log('created IoPlus');
-	
-		//default
 		//0x20 32 IC1
 		//0x21 33 IC2
 		node.bus = config.bus;
-
-		var client = new Iopi(config.bus);
+		node.log('created IoPlus');
 	}
 
 	RED.nodes.registerType("IoPlus", nodeIoPlus);
 
+	// IoPlus out
+	function ioPlusOutNode(config) {
+		RED.nodes.createNode(this, config);
+		this.ioPlus = RED.nodes.getNode(config.ioPlus);
+
+		if (this.ioPlus) {
+			var node = this;
+			//node.port = serialPool.get(this.serialConfig.serialport,
+			//    this.serialConfig.serialbaud,
+			//    this.serialConfig.newline);
+			//node.addCh = "";
+			//if (node.serialConfig.addchar == "true" || node.serialConfig.addchar === true) {
+			//    node.addCh = this.serialConfig.newline.replace("\\n", "\n").replace("\\r", "\r").replace("\\t", "\t").replace("\\e", "\e").replace("\\f", "\f").replace("\\0", "\0"); // jshint ignore:line
+			//}
+			node.on("input", function (msg) {
+				if (msg.hasOwnProperty("payload")) {
+					var payload = msg.payload;
+					if (!Buffer.isBuffer(payload)) {
+						if (typeof payload === "object") {
+							payload = JSON.stringify(payload);
+						}
+						else {
+							payload = payload.toString();
+						}
+						payload += node.addCh;
+					}
+				//    else if (node.addCh !== "") {
+				//        payload = Buffer.concat([payload, new Buffer(node.addCh)]);
+				//    }
+				//    node.port.write(payload, function (err, res) {
+				//        if (err) {
+				//            var errmsg = err.toString().replace("Serialport", "Serialport " + node.port.serial.path);
+				//            node.error(errmsg, msg);
+				//        }
+					//});
+				}
+			});
+			node.port.on('ready', function () {
+				node.status({ fill: "green", shape: "dot", text: "node-red:common.status.connected" });
+			});
+			node.port.on('closed', function () {
+				node.status({ fill: "red", shape: "ring", text: "node-red:common.status.not-connected" });
+			});
+		}
+		else {
+			this.error(RED._("serial.errors.missing-conf"));
+		}
+
+		this.on("close", function (done) {
+			//clean up node
+
+			//if (this.serialConfig) {
+			//    serialPool.close(this.serialConfig.serialport, done);
+			//}
+			//else {
+				done();
+			//}
+		});
+	}
+	RED.nodes.registerType("IoPlus out", ioPlusOutNode);
+
+	// IoPlus in
+	function ioPlusInNode(config) {
+		RED.nodes.createNode(this, config);
+		this.ioPlus = config.ioplus;
+		this.ioPlusConfig = RED.nodes.getNode(this.ioPlus);
+
+		var client = new Iopi(config.bus);
+
+		if (this.ioPlusConfig) {
+			var node = this;
+			node.bus = config.bus;
+			// all
+			node.bus.setPortDirection(0, 0xFF); // set to inputs
+			node.bus.setPortPullups(0, 0xFF); // enable internal pull-up resistors
+
+			node.log('Pin 1: %d', node.bus.readPin(1));
 
 
-	//// We will read the inputs 1 to 8 from bus 1 so set port 0 to be inputs and
-	//// enable the internal pull-up resistors
-
-	//bus1.setPortDirection(0, 0xFF);
-	//bus1.setPortPullups(0, 0xFF);
-
-	//// Set port 0 to be outputs
-	//bus1.setPortDirection(0, 0x00);
-
-	
-
-
-
-
-
-
-
-	//function serialOutNode(config) {
-	//    RED.nodes.createNode(this, config);
-	////    this.serial = config.serial;
-	////    this.serialConfig = RED.nodes.getNode(this.serial);
-
-	//    if (this.serialConfig) {
-	//        var node = this;
-	//        //node.port = serialPool.get(this.serialConfig.serialport,
-	//        //    this.serialConfig.serialbaud,
-	//        //    this.serialConfig.databits,
-	//        //    this.serialConfig.parity,
-	//        //    this.serialConfig.stopbits,
-	//        //    this.serialConfig.newline);
-	//        //node.addCh = "";
-	//        //if (node.serialConfig.addchar == "true" || node.serialConfig.addchar === true) {
-	//        //    node.addCh = this.serialConfig.newline.replace("\\n", "\n").replace("\\r", "\r").replace("\\t", "\t").replace("\\e", "\e").replace("\\f", "\f").replace("\\0", "\0"); // jshint ignore:line
-	//        //}
-	//        node.on("input", function (msg) {
-	//            if (msg.hasOwnProperty("payload")) {
-	//                var payload = msg.payload;
-	//                if (!Buffer.isBuffer(payload)) {
-	//                    if (typeof payload === "object") {
-	//                        payload = JSON.stringify(payload);
-	//                    }
-	//                    else {
-	//                        payload = payload.toString();
-	//                    }
-	//                    payload += node.addCh;
-	//                }
-	//            //    else if (node.addCh !== "") {
-	//            //        payload = Buffer.concat([payload, new Buffer(node.addCh)]);
-	//            //    }
-	//            //    node.port.write(payload, function (err, res) {
-	//            //        if (err) {
-	//            //            var errmsg = err.toString().replace("Serialport", "Serialport " + node.port.serial.path);
-	//            //            node.error(errmsg, msg);
-	//            //        }
-	//                //});
-	//            }
-	//        });
-	//        node.port.on('ready', function () {
-	//            node.status({ fill: "green", shape: "dot", text: "node-red:common.status.connected" });
-	//        });
-	//        node.port.on('closed', function () {
-	//            node.status({ fill: "red", shape: "ring", text: "node-red:common.status.not-connected" });
-	//        });
-	//    }
-	//    else {
-	//        this.error(RED._("serial.errors.missing-conf"));
-	//    }
-
-	//    this.on("close", function (done) {
-	//        //clean up node
-
-	//        //if (this.serialConfig) {
-	//        //    serialPool.close(this.serialConfig.serialport, done);
-	//        //}
-	//        //else {
-	//            done();
-	//        //}
-	//    });
-	//}
-	//RED.nodes.registerType("serial out", serialOutNode);
+			this.port.on('data', function (msg) {
+				// single char buffer
+				if ((node.serialConfig.newline === 0) || (node.serialConfig.newline === "")) {
+					if (node.serialConfig.bin !== "bin") { node.send({ "payload": String.fromCharCode(msg) }); }
+					else { node.send({ "payload": new Buffer([msg]) }); }
+				}
+			});
+			this.port.on('ready', function () {
+				node.status({ fill: "green", shape: "dot", text: "node-red:common.status.connected" });
+			});
+			this.port.on('closed', function () {
+				node.status({ fill: "red", shape: "ring", text: "node-red:common.status.not-connected" });
+			});
+		}
+		else {
+			this.error(RED._("serial.errors.missing-conf"));
+		}
+		this.on("close", function (done) {
+			//if (this.serialConfig) {
+			//	serialPool.close(this.serialConfig.serialport, done);
+			//}
+			//else {
+				done();
+			//}
+		});
+	}
+	RED.nodes.registerType("IoPlus in", ioPlusInNode);
 }
