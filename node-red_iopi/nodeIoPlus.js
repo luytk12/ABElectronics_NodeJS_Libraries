6bbdc32987093c9f1e7daecbfb8fea034ac5cd2f
Koen Luyten <koenluyten@hotmail.com>
2017-12-01 08:55:13 +0100

    read/write ok

--- node-red_iopi/nodeIoPlus.js
+++ node-red_iopi/nodeIoPlus.js
@@ -23,28 +23,25 @@ module.exports = function (RED) {
         this.ioPlusConfig = RED.nodes.getNode(config.IoPlus);
         if (this.ioPlusConfig) {
             var node = this;
-            node.log('boe');
             node.on("input", function (msg) {
-                // do write shit
                 if (msg.hasOwnProperty("payload")) {
-                    var payload = msg.payload;
-                    if (!Buffer.isBuffer(payload)) {
-                        if (typeof payload === "object") {
-                            payload = JSON.stringify(payload);
-                        }
-                        else {
-                            payload = payload.toString();
-                        }
-                        payload += node.addCh;
+                    var pin = msg.payload['pin'] || null;
+                    var value = (msg.payload['value'] === true);
+                    if ((typeof (value) === 'boolean') && (pin != null && (typeof (pin) == 'number') && (pin >= 1 && pin <= 16))) {
+                        var bus = parseInt(this.ioPlusConfig.bus);
+                        node.log("bus: " + bus + " pin: " + pin + " value: " + value);
+
+                        var client = new IoPi(bus);
+                        client.setPortDirection(0, 0x00); // set to output
+                        client.setPortPullups(0, 0x00); // disable internal pull-up resistors	                       
+                        var writeResult = client.writePin(msg.pin, msg.value);
+                        node.log("writeResult: " + writeResult);
                     }
+                    else { node.warn('IoPi: incorrect pin or value!'); }
                 }
+                else { node.warn('IoPi: no payload'); }
             });
-            //	node.port.on('ready', function () {
-            //		node.status({ fill: "green", shape: "dot", text: "node-red:common.status.connected" });
-            //	});
-            //	node.port.on('closed', function () {
-            //		node.status({ fill: "red", shape: "ring", text: "node-red:common.status.not-connected" });
-            //	});
+
         }
         else {
             this.error(RED._("IoPlus.errors.missing-conf"));
@@ -62,34 +59,19 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         this.ioPlusConfig = RED.nodes.getNode(config.IoPlus);
 
-        var client = new IoPi(0x20);
-
         if (this.ioPlusConfig) {
             var node = this;
-            node.log('en ba');
-            node.log(this.ioPlusConfig.bus);
-            // all
+            var pin = parseInt(config.pin);
+            if (pin != null && (typeof (pin) == 'number') && (pin >= 1 && pin <= 16)) {
+                var bus = parseInt(this.ioPlusConfig.bus);
+                node.log("bus: " + bus + " pin: " + pin);
 
-            cclient.setPortDirection(0, 0xFF); // set to inputs
-
-            node.bus.setPortPullups(0, 0xFF); // enable internal pull-up resistors
-
-            node.log('Pin 1: %d', node.bus.readPin(1));
-
-
-            this.port.on('data', function (msg) {
-                // single char buffer
-                if ((node.serialConfig.newline === 0) || (node.serialConfig.newline === "")) {
-                    if (node.serialConfig.bin !== "bin") { node.send({ "payload": String.fromCharCode(msg) }); }
-                    else { node.send({ "payload": new Buffer([msg]) }); }
-                }
-            });
-            this.port.on('ready', function () {
-                node.status({ fill: "green", shape: "dot", text: "node-red:common.status.connected" });
-            });
-            this.port.on('closed', function () {
-                node.status({ fill: "red", shape: "ring", text: "node-red:common.status.not-connected" });
-            });
+                var client = new IoPi(bus);
+                client.setPortDirection(0, 0xFF); // set to input
+                client.setPortPullups(0, 0xFF); // enable the internal pull-up resistors
+                node.log("readpin: " + client.readPin(pin));
+            }
+            else { node.warn('IoPi: incorrect pin or value!'); }
         }
         else {
             this.error(RED._("IoPlus.errors.missing-conf"));
